00630952cb0861355fc1d35bfaa451b67ecbf6b6
Touseef Elahi <touseefelahi@ymail.com>
2022-09-29 11:28:51 +0300

    Add high limit for test sample

--- TestApp/MainWindow.xaml.cs
+++ TestApp/MainWindow.xaml.cs
@@ -14,24 +14,46 @@ namespace TestApp
         public MainWindow()
         {
             InitializeComponent();
-            logger = new Logger.Core.Logger();
-            logger.LogLimit = 5;
+            int loopCount = 50_000;
+            int delayedLoopCount = 100;
+            logger = new Logger.Core.Logger
+            {
+                LogLimit = 500000,
+            };
             LogView.DataContext = new Logger.Core.LogControlViewModel(logger);
             logger.Log("Test App working");
             logger.Log("Test1 App working");
             logger.Log("Test2 App working");
+
             Task.Run(async () =>
             {
-                logger.Log("Test3 App working");
-                await Task.Delay(300).ConfigureAwait(false);
-                logger.Log("Test4 App working");
-                await Task.Delay(300).ConfigureAwait(false);
-                ShowException();
-                await Task.Delay(300).ConfigureAwait(false);
-                logger.Log("Test5 App working");
-                await Task.Delay(300).ConfigureAwait(false);
-                logger.Log("Test6 App working");
-                await Task.Delay(300).ConfigureAwait(false);
+                for (int i = 0; i < delayedLoopCount; i++)
+                {
+                    logger.Log($"Delayed loop test - from {i} task");
+                    await Task.Delay(10);
+                    ShowException();
+                }
+            });
+            Task.Run(() =>
+            {
+                Parallel.For(0, loopCount, (a) =>
+                {
+                    logger.Log($"First loop test - from {a} task");
+                });
+            });
+            Task.Run(() =>
+            {
+                Parallel.For(0, loopCount, (a) =>
+                {
+                    logger.Log($"Second loop test - from {a} task");
+                });
+            });
+            Task.Run(() =>
+            {
+                Parallel.For(0, loopCount, (a) =>
+                {
+                    logger.Log($"Third loop test - from {a} task");
+                });
             });
         }
 
